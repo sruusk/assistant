38062dd655d7b79ceb6bf43c0be7acce710fe349
sruusk <57395313+sruusk@users.noreply.github.com>
2024-11-25 21:24:35 +0200

    test fix

--- plugins/crypto.ts
+++ plugins/crypto.ts
@@ -1,6 +1,6 @@
 export default defineNuxtPlugin(() => {
   if (process.server) {
-    const crypto = require('crypto');
+    const crypto = require('~/plugins/crypto');
     global.crypto = crypto;
   }
 })
