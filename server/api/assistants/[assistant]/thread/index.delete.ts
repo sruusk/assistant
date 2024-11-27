7a3252051d16327b6663229e29b32417d56431c7
sruusk <57395313+sruusk@users.noreply.github.com>
2024-11-27 20:27:44 +0200

    Cleanup uploaded files on thread delete

--- server/api/assistants/[assistant]/thread/index.delete.ts
+++ server/api/assistants/[assistant]/thread/index.delete.ts
@@ -3,8 +3,23 @@ export default defineAssistantAuthenticatedHandler(async (event) => {
   const userThread = await event.context.storage.getItem(`user:${event.context.logtoUser.sub}:thread:${assistant}`);
 
   if(userThread) {
+    // Delete all attached file first
+    const messages: any[] = await $fetch(`/api/assistants/${assistant}/thread/messages`, {
+      method: 'GET',
+      headers: getRequestHeaders(event) as HeadersInit,
+    });
+    const files = messages
+      .map((m) => m.content)
+      .flat()
+      .filter((c) => c.type === 'image_file')
+      .map((c) => c.image_file.file_id);
+
+    await Promise.all(files.map(async (fileId) => {
+      await event.context.openai.files.del(fileId);
+    }));
+
     const response = await event.context.openai.beta.threads.del(userThread);
-    console.log('Deleting thread', userThread, response);
+    console.log('Deleted thread', userThread, response);
     return response;
   }
   else {
